834cce9b6f9de8e8752b99ceaeacf643aa6a6132
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-08-12 20:00:22 +0300

    4 tasks

--- js/third-week.js
+++ js/third-week.js
@@ -1,1 +1,79 @@
+// 1.Присвой змінній apartment об'єкт, який описує квартиру з наступними характеристиками:
+// imgUrl - фотографія, значення "https://via.placeholder.com/640x480";
+// descr - опис, значення "Spacious apartment in the city center";
+// rating - рейтинг, значення 4;
+// price - ціна, значення 2153;
+// tags - метаінформація, масив ["premium", "promoted", "top"]
 
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+};
+
+// 2.Доповни об'єкт квартири властивістю owner, значенням якого буде об'єкт з інформацією про власника. Додай йому наступні властивості:
+// name - ім'я власника, значення "Henry";
+// phone - телефон, значення "982-126-1588";
+// email - пошта, значення "henry.carter@aptmail.com".
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+  owner: {
+    name: 'Henry',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  },
+};
+
+//  3.Доповни код, присвоївши оголошеним змінним вирази звернення до відповідних властивостей об'єкта apartment.
+//  aptRating - рейтинг;
+//  aptDescr - опис;
+//  aptPrice - ціна;
+//  aptTags - теги.
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+};
+
+const aptRating = apartment.rating;
+const aptDescr = apartment.descr;
+const aptPrice = apartment.price;
+const aptTags = apartment.tags;
+
+// 4.Доповни код, присвоївши оголошеним змінним вирази звернення до відповідних властивостей об'єкта apartment.
+// ownerName - ім'я власника;
+// ownerPhone - телефон власника;
+// ownerEmail - пошта власника;
+// numberOfTags - кількість елементів масиву у властивості tags;
+// firstTag - перший елемент масиву у властивості tags;
+// lastTag - останній елемент масиву у властивості tags.
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+  owner: {
+    name: 'Henry',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  },
+};
+
+const ownerName = apartment.owner.name;
+const ownerPhone = apartment.owner.phone;
+const ownerEmail = apartment.owner.email;
+const numberOfTags = apartment.tags.length;
+const firstTag = apartment.tags[0];
+const lastTag = apartment.tags[apartment.tags.length - 1];
