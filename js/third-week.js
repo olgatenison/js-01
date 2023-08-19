76f9ec0bdd174315255e2467895f91e0181e866e
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-08-19 13:07:38 +0300

    add tasks

--- js/third-week.js
+++ js/third-week.js
@@ -77,3 +77,131 @@ const ownerEmail = apartment.owner.email;
 const numberOfTags = apartment.tags.length;
 const firstTag = apartment.tags[0];
 const lastTag = apartment.tags[apartment.tags.length - 1];
+
+// 5.Доповни код, присвоївши оголошеним змінним вирази звернення до відповідних властивостей об'єкта apartment, використовуючи синтаксис «квадратних дужок».
+// aptRating - рейтинг;
+// aptDescr - опис;
+// aptPrice - ціна;
+// aptTags - теги
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+};
+
+const aptRating = apartment['rating'];
+const aptDescr = apartment['descr'];
+const aptPrice = apartment['price'];
+const aptTags = apartment['tags'];
+
+// 6.Доповни код, оновивши значення властивостей об'єкта apartment:
+// ціну у властивості price на 5000;
+// рейтинг квартири у властивості rating на 4.7;
+// ім'я власника у вкладеній властивості name на "Henry Sibola";
+// масив тегів у властивості tags, додавши в кінець рядок "trusted".
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+  owner: {
+    name: 'Henry',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  },
+};
+apartment.owner.name = 'Henry Sibola';
+apartment.price = 5000;
+apartment.rating = 4.7;
+apartment.tags.push('trusted');
+
+// 7.Додай об'єкту apartment кілька нових властивостей:
+// area - площа в квадратних метрах, число 60;
+// rooms - кількість кімнат, число 3;
+// location - місце розташування квартири, об'єкт з наступними вкладеними властивостями;
+// country - країна, рядок "Jamaica";
+// city - місто, рядок "Kingston".
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4.7,
+  price: 5000,
+  tags: ['premium', 'promoted', 'top', 'trusted'],
+  owner: {
+    name: 'Henry Sibola',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  },
+};
+apartment.area = 60;
+apartment.rooms = 3;
+apartment.location = { country: 'Jamaica', city: 'Kingston' };
+
+// 8.Доповни код оголошення об'єкта таким чином, щоб у нього були властивості name, price, image і tags зі значеннями зі змінних з аналогічними іменами. Обов'язково використовуй синтаксис коротких властивостей.
+
+const name = 'Repair Droid';
+const price = 2500;
+const image = 'https://via.placeholder.com/640x480';
+const tags = ['on sale', 'trending', 'best buy'];
+
+const product = {
+  name,
+  price,
+  image,
+  tags,
+};
+
+// 9.Доповни код оголошення об'єкта credentials таким чином, щоб в результаті у нього були дві властивості: email і password, імена яких зберігаються у змінних emailInputName і passwordInputName.
+// Значенням властивості email повинен бути рядок "henry.carter@aptmail.com", а значенням властивості password - рядок "jqueryismyjam".
+
+const emailInputName = 'email';
+const passwordInputName = 'password';
+
+const credentials = {
+  [emailInputName]: 'henry.carter@aptmail.com',
+  [passwordInputName]: 'jqueryismyjam',
+};
+
+// 10.Перебери об'єкт apartment, використовуючи цикл for...in, і запиши в масив keys всі його ключі, а в масив values всі значення його властивостей.
+
+const apartment = {
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+};
+const keys = [];
+const values = [];
+// Change code below this line
+for (key in apartment) {
+  keys.push(key);
+}
+for (value in apartment) {
+  values.push(apartment[value]);
+}
+
+// 11.Виконай рефакторинг рішення попереднього завдання, додавши в цикл for...in перевірку на власну властивість.
+
+const keys = [];
+const values = [];
+const advert = {
+  service: 'apt',
+};
+const apartment = Object.create(advert);
+apartment.descr = 'Spacious apartment in the city center';
+apartment.rating = 4;
+apartment.price = 2153;
+
+for (const key in apartment) {
+  if (apartment.hasOwnProperty(key)) {
+    keys.push(key);
+  }
+  if (apartment.hasOwnProperty(key)) {
+    values.push(apartment[key]);
+  }
+}
